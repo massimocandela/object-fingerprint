77112b54033f34d6423a37eddfb3a04235a0071e
Massimo Candela <massimo@us.ntt.net>
2023-10-25 15:57:37 +0200

    minor license changes

--- src/fingerprint.js
+++ src/fingerprint.js
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2022 Massimo Candela <https://massimocandela.com>
+ * Copyright (c) 2023 Massimo Candela <https://massimocandela.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
